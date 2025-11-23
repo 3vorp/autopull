fb6fbd089d593622974af19dadf2d84d73116c76
mullak99 <kjblakey@gmail.com>
2025-11-23 03:28:57 +0000

    Add Windows Support (#1)
    * feat: add Windows support and VBS script for running it in the Task Scheduler
    
    ---------
    
    Co-authored-by: Evorp <75297863+3vorp@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 const { join } = require("node:path");
 const { exec } = require("node:child_process");
+
+/** @type {{ basePath: string, items: string[] }[] } */
 const config = require("./config.json");
 
+/**
+ * Check whether a path starts either with a slash (*nix) or drive letter (Windows)
+ * @author Evorp, mullak99
+ * @param {string} path
+*/
+const isAbsolute = (path) => path.startsWith("/") || /^[A-Za-z]:\\/.test(path);
+const userHome = process.env.HOME || process.env.USERPROFILE;
+
 for (const { basePath, items } of config) {
+	const path = join(isAbsolute(basePath) ? basePath : join(userHome, basePath));
 	for (const item of items) {
-		const cwd = join(
-			basePath.startsWith("/") ? basePath : join(process.env.HOME, basePath),
-			item,
-		);
+		const cwd = join(path, item);
 		exec("git stash && git pull && git stash pop; git stash drop", { cwd }, (err, out) =>
 			console.log(`=== ${item} ===\n${out || err}`),
 		);
